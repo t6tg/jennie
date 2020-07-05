185f255547dd7855dd13775e07fac7ea9dff1596
thanawatgulati <thanawatgulatijames@gmail.com>
2020-07-05 17:55:16 +0700

    pwa config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,29 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [`gatsby-plugin-postcss`],
+  plugins: [
+    `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Kim Jennie`,
+        short_name: `Jennie`,
+        start_url: `/`,
+        background_color: `#1A202C`,
+        theme_color: `#EFABC0`,
+        display: `standalone`,
+        icon: `static/icon.png`,
+        icons: [
+          {
+            src: `/favicons/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+  ],
 }
