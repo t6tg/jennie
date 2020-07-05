2e40fcc565d64edae9b92b4fdcefb92f79c9ab76
thanawatgulati <thanawatgulatijames@gmail.com>
2020-07-05 22:39:17 +0700

    html Lang

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const Layout = (props: Props) => {
   return (
-    <html>
+    <html lang="en">
       <div className="container mx-auto w-4/5 mt-2 xl:mt-8">
         <Navbar tag={props.tag} />
         {props.children}
