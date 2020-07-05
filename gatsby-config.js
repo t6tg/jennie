1435c2b8d451696c81ab121694c2c3a39d4c5c10
thanawatgulati <thanawatgulatijames@gmail.com>
2020-07-05 22:37:02 +0700

    PWA Fix Manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,11 +24,13 @@ module.exports = {
             src: `/favicons/android-chrome-192x192.png`,
             sizes: `192x192`,
             type: `image/png`,
+            purpose: "any maskable",
           },
           {
             src: `/favicons/android-chrome-512x512.png`,
             sizes: `512x512`,
             type: `image/png`,
+            purpose: "any maskable",
           },
         ],
       },
